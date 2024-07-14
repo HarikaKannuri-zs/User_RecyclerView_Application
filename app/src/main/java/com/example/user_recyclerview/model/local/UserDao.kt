704b49e6219fbba3e635d81d561a2d99973c41a6
Raramuri <harika.kannuri@zopsmart.com>
2024-07-14 16:15:02 +0530

    ref #8-implementing-mvvm-architecture

--- app/src/main/java/com/example/user_recyclerview/model/local/UserDao.kt
+++ app/src/main/java/com/example/user_recyclerview/model/local/UserDao.kt
@@ -1,17 +1,14 @@
-package com.example.user_recyclerview
+package com.example.user_recyclerview.model.local
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 
 @Dao
 interface UserDao {
-
     @Insert
     fun insertUser(user: User)
 
     @Query("SELECT * FROM user")
     fun getAllUsers(): List<User>
-
 }
--- app/src/main/java/com/example/user_recyclerview/model/remote/ApiService.kt
+++ app/src/main/java/com/example/user_recyclerview/model/remote/ApiService.kt
@@ -1,4 +1,4 @@
-package com.example.user_recyclerview.model
+package com.example.user_recyclerview.model.remote
 
 import retrofit2.Call
 import retrofit2.http.GET
